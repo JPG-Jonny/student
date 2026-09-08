97906f3e0032b4b9f46d225e4288801089ecf0c2
not_a_bot <niladridey121212@gmail.com>
2026-09-09 02:11:20 +0530

    Return access token in response for registration

--- backend/main.py
+++ backend/main.py
@@ -573,7 +573,10 @@ def faculty_register(data: FacultyRegister, db: Session = Depends(get_db)):
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Email already registered"
         )
-    
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
     avatar = "".join([n[0].upper() for n in data.name.split()])
     new_faculty = Faculty(
         name=data.name,
